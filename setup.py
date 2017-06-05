ec53737ee1df3eb9e43add537f59721c1dd7f7b5
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-05 11:07:14 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='nanoget',
+    version='0.9.0',
+    description='Functions to extract information from Oxford Nanopore sequencing data and alignments.',
+    long_description='Functions to extract information from Oxford Nanopore sequencing data and alignments.',
+    url='https://github.com/wdecoster/nanoget',
+    author='Wouter De Coster',
+    author_email='decosterwouter@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='nanopore sequencing plotting quality control',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'biopython', 'pysam>0.10.0.0', 'nanomath'],
+    package_dir={'nanoget': 'nanoget'})
