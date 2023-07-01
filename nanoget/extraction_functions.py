8bc0d5b7eb1ad5d1aa57af885f6d0f53c606fcd9
wdecoster <decosterwouter@gmail.com>
2023-07-01 12:54:57 +0200

    also fix for fastq_minimal

--- nanoget/extraction_functions.py
+++ nanoget/extraction_functions.py
@@ -541,5 +541,5 @@ def process_fastq_minimal(fastq, **kwargs):
     except IndexError:
         logging.error("Fatal: Incorrect file structure for fastq_minimal")
         sys.exit("Error: file does not match expected structure for fastq_minimal")
-    df["timestamp"] = df["timestamp"].astype("datetime64[ns]")
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
     return ut.reduce_memory_usage(df)
